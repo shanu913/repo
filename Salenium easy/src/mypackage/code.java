1959bcabb965f746465bfafd39551302d7f72e25
shanu913 <shanukumar5294@gmail.com>
2017-12-11 00:35:42 +0530

    code

--- Salenium easy/src/mypackage/code.java
+++ Salenium easy/src/mypackage/code.java
@@ -132,3 +132,18 @@ WebElement elementToClick = driver.findElement(By.xpath("Your xpath"));
 //Click the element
 
 elementToClick.click();
+
+
+
+WebElement input = driver.findElement(By.id("ADMServerPanel1_ctl02_ctl05_ctl06_btnSearch"));
+new Actions(driver).moveToElement(input).click().perform();
+
+
+
+driver.get("http://qtpselenium.com/");
+driver.findElement(By.xpath("//button[contains(.,'Member Login')]")).click();
+WebDriverWait wait = new WebDriverWait(driver, 5); // create a WebDriverWait that we will reuse
+wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(.,'Login to Selenium Account')]"))).click();
+wait.until(ExpectedConditions.presenceOfElementLocated(By.id("email"))).sendKeys("Some Email ID");
+driver.findElement(By.id("login-password")).sendKeys("Some Password");
+driver.findElement(By.xpath("//button[contains(.,'Login')]")).click();
