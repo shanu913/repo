9535aea4a654a8d3b6074cdd7c8744a80e4f0eb6
Admin <Admin@192.168.1.19>
2017-12-22 12:03:13 +0530

    Negative scenario

--- Salenium easy/src/mypackage/code.java
+++ Salenium easy/src/mypackage/code.java
@@ -162,4 +162,62 @@ driver.findElement(By.xpath("Your xpath")).click();   //After Explicit Wait Clic
 // Check the title of the page
 System.out.println("Page title is: " + driver.getTitle());
 
-hi world tt
+
+Negative Scenarios:
+
+1) Valid User name and Invalid Password
+2) Invalid user Name and Valid Password
+3) Blank User name and Valid Password/Invalid Password
+4) Valid / Invalid User name and Blank password
+5) Blank User name and Blank password
+-----------------------------------
+Selenium Test Case:
+
+public class VerifyLogin {
+static int i;
+static String username, password, iteration;
+public static void main(String[] args) throws InterruptedException {
+WebDriver driver = new FirefoxDriver();
+
+for (i = 1; i <= 2; i++){
+if (i == 1)    {
+username = "admin";
+password = "admin@123";
+iteration = "Iteration 1:";
+}
+else if(i == 2){
+username = "admina";
+password = "admin@123a";
+iteration = "Iteration 2:";    
+}
+
+driver.get("http://www.gcrit.com/build3/admin/");
+driver.findElement(By.name("username")).sendKeys(username);
+driver.findElement(By.name("password")).sendKeys(password);
+Thread.sleep(3000);
+driver.findElement(By.id("tdb1")).click();
+
+String url = driver.getCurrentUrl();
+
+if (url.contains("http://www.gcrit.com/build3/admin/index.php")){
+System.out.println(iteration + ("Input Data: ") + username + ", "+ password + " Admin Login 
+
+Successful -Passed");
+driver.findElement(By.linkText("Logoff")).click();
+Thread.sleep(2000);
+}
+else if (! url.contains("http://www.gcrit.com/build3/admin/index.php")){
+String ErrorMessage = driver.findElement(By.className("messageStackError")).getText();
+if (ErrorMessage.contains("Invalid administrator login attempt.")){
+System.out.println(iteration + ("Input Data: ") + username + ", "+ password + " Handling Invalid 
+
+Inputs -Passed");
+}
+else {
+System.out.println(iteration +  "- Failed");
+}
+}
+}
+driver.close();
+}
+}
