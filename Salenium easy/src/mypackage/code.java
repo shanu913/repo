cf791286083d2ff141cc91268e3cb08eb7ec63d8
Admin <Admin@192.168.1.3>
2017-12-26 19:16:55 +0530

    Commit

--- Salenium easy/src/mypackage/code.java
+++ Salenium easy/src/mypackage/code.java
@@ -298,3 +298,51 @@ public class VerifyLogin {
 	}
 	}
 	}
+
+
+public class VerifyLogin {
+	public static String Error_Message, username, password, iteration;
+
+	public static void main(String[] args) {
+	VerifyLogin ob =  new VerifyLogin();
+		for (int i =1; i <=3; i++){
+			if (i == 1){
+				username ="admin";
+				password="admin@123";
+				ob.Run(username, password);
+			} else if (i == 2) {
+				username ="admin1";
+				password="admin@123";
+				ob.Run(username, password);
+			} else if (i == 3) {
+				username ="admin1";
+				password="admin@123";
+				ob.Run(username, password);
+			}
+		}
+	}
+	
+	public void Run(String username, String password) {
+		System.setProperty("webdriver.gecko.driver","D:\\gecko\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.gcrit.com/build3/admin/");
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.id("tdb1")).click();
+
+		String URL = driver.getCurrentUrl();
+		if (! URL.equals("http://www.gcrit.com/build3/admin/index.php")){
+			System.out.println("Failed");  
+			
+		//Error_Message = driver.findElement(By.className("messageStackError")).getText();
+		}
+
+		if (URL.equals("http://www.gcrit.com/build3/admin/index.php")) {
+		System.out.println(iteration+" - Admin Login Successful -Passed");    
+		}    
+		else if ((! URL.equals("http://www.gcrit.com/build3/admin/index.php"))){
+		System.out.println(iteration+" -Admin Login Unsuccessful and Showing Correct Error Message-Failed");
+		}
+		driver.close();
+	}
+	}
