021f33808d2e5d52269a3cff6a823d8c1a6a26ae
shanu913 <shanukumar5294@gmail.com>
2017-12-27 01:19:15 +0530

    commit

--- Salenium easy/src/mypackage/code.java
+++ Salenium easy/src/mypackage/code.java
@@ -1,8 +1,12 @@
 package mypackage;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class code {
 
@@ -346,3 +350,20 @@ public class VerifyLogin {
 		driver.close();
 	}
 	}
+
+
+
+Teamwear
+
+System.setProperty("webdriver.gecko.driver","E:\\Study\\Selenium\\New folder\\geckodriver.exe");
+WebDriver driver=new FirefoxDriver();
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+driver.get("http://iskdemo.com/teamwear/index.php/");
+driver.findElement(By.xpath("//a[text()='Sign Up'and @class='login']")).click();
+Thread.sleep(10000);
+WebDriverWait wait = new WebDriverWait(driver, 40);
+wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit'and @title='Register']")));
+driver.findElement(By.xpath("//button[@type='submit'and @title='Register']")).click();
+driver.findElement(By.xpath("//*[@id='firstname']")).sendKeys("Test");
+driver.findElement(By.xpath("//*[@id='lastname']")).sendKeys("User");
+driver.findElement(By.xpath("//*[@id='email_address']")).sendKeys("t.user.com");
