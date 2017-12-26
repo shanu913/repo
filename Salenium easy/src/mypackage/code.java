e4b73b14efca485a3a1a5b5be361936ecaad8e1a
Admin <Admin@192.168.1.3>
2017-12-26 17:29:58 +0530

    commit

--- Salenium easy/src/mypackage/code.java
+++ Salenium easy/src/mypackage/code.java
@@ -1,5 +1,8 @@
 package mypackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class code {
 
@@ -255,4 +258,21 @@ xpath using class and text
 //a[@class='headerLink' and text()='Logoff']
 
 
-changes
+WebDriver driver = new FirefoxDriver();
+driver.get("http://www.gcrit.com/build3/admin/");
+driver.findElement(By.name("username")).sendKeys(username);
+driver.findElement(By.name("password")).sendKeys(password);
+driver.findElement(By.id("tdb1")).click();
+
+String URL = driver.getCurrentUrl();
+if (! URL.equals("http://www.gcrit.com/build3/admin/index.php")){
+	System.out.println("Failed");  
+	
+//Error_Message = driver.findElement(By.className("messageStackError")).getText();
+}
+
+if (URL.equals("http://www.gcrit.com/build3/admin/index.php")) {
+System.out.println(iteration+" - Admin Login Successful -Passed");    
+}    
+else if ((! URL.equals("http://www.gcrit.com/build3/admin/index.php"))){
+System.out.println(iteration+" -Admin Login Unsuccessful and Showing Correct Error Message-Failed");
