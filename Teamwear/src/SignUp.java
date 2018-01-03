3a75b3bc12e99e78624a5824fb5c26eb74d2237f
shanu913 <shanukumar5294@gmail.com>
2018-01-04 02:10:07 +0530

    commit

--- Teamwear/src/SignUp.java
+++ Teamwear/src/SignUp.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SignUp {
-	public static String FirstName, LastName, EmailAddress;
+	public static String FirstName, LastName, EmailAddress, Error_Message;
 	public static Boolean click;
 	public static void main(String[] args)
 	{
-	System.setProperty("webdriver.gecko.driver","D:\\gecko\\geckodriver.exe");
+	System.setProperty("webdriver.gecko.driver", "E:\\Study\\Selenium\\New folder\\geckodriver.exe");
 	for (int i =1; i <=2; i++){
 		if (i == 1){
 			FirstName ="use";
@@ -44,8 +44,18 @@ public class SignUp {
 	wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit'and @title='Register']")));
 	driver.findElement(By.xpath("//button[@type='submit'and @title='Register']")).click();
 	
-	
-		}
+	String url=driver.getCurrentUrl();
+	if (!url.equals("http://iskdemo.com/teamwear/index.php/customer/account/index/"))
+	{
+		System.out.println("Test cases failed");
 	}
+	if (url.equals("http://iskdemo.com/teamwear/index.php/customer/account/index/"))
+	{
+		System.out.println("Test cases passed");
+	}
+	else if (! url.equals("http://iskdemo.com/teamwear/index.php/customer/account/index/")) && (Error_Message.contains("There is already an account with this email address. If you are sure that it is your email address, click here to get your password and access your account.")){
+	driver.close();
+		}
+	}  
 	}
 
