b1cb503655bc186aba4a477cf5211c94543e9690
Admin <Admin@192.168.1.13>
2018-01-09 19:16:14 +0530

    Commit

--- Teamwear/src/SignUp.java
+++ Teamwear/src/SignUp.java
@@ -8,18 +8,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SignUp {
 	WebDriver driver;
-	public static String FirstName, LastName, EmailAddress, Error_Message;
+	public static String FirstName, LastName, EmailAddress;
 	public static Boolean click;
 	public static int iteration;
-	
-	public void invokebroswer()
-	{
-		System.setProperty("webdriver.gecko.driver", "E:\\Study\\Selenium\\New folder\\geckodriver.exe");
+
+	public void invokebroswer() {
+		System.setProperty("webdriver.gecko.driver", "D:\\gecko\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("http://iskdemo.com/teamwear/index.php/");
 	}
-
 
 	public void register() throws InterruptedException {
 		for (int i = 1; i <= 2; i++) {
@@ -28,10 +26,10 @@ public class SignUp {
 				FirstName = "use";
 				LastName = "us";
 				click = true;
-				EmailAddress = "a27@gmail.com";
+				EmailAddress = "aa4@gmail.com";
 				iteration = 1;
 				Thread.sleep(5000);
-				
+
 			} else if (i == 2) {
 				FirstName = "aad";
 				LastName = "use";
@@ -40,8 +38,6 @@ public class SignUp {
 				iteration = 2;
 			}
 
-				
-			
 			driver.findElement(By.xpath("//a[text()='Sign Up'and @class='login']")).click();
 			driver.findElement(By.xpath("//*[@id='firstname']")).sendKeys(FirstName);
 			driver.findElement(By.xpath("//*[@id='lastname']")).sendKeys(LastName);
@@ -60,11 +56,10 @@ public class SignUp {
 			wait1.until(
 					ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit'and @title='Register']")));
 			driver.findElement(By.xpath("//button[@type='submit'and @title='Register']")).click();
-			
 			Thread.sleep(10000);
 
-
 			String title = driver.getTitle();
+			System.out.println(title);
 
 			if (title.equals("My Account")) {
 				System.out.println("iteration " + iteration + " Test cases passed");
@@ -72,24 +67,40 @@ public class SignUp {
 				System.out.println("iteration " + iteration + " Test cases failed");
 			}
 
-		} 
+		}
 	}
-	public void login() throws InterruptedException
-	{
+
+	public void login() throws InterruptedException {
 		invokebroswer();
 		driver.findElement(By.xpath("//a[@class='login' and text()='Login']")).click();
 		driver.findElement(By.xpath("//input[@id='email']")).clear();
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("a20@gmail.com");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("aa4@gmail.com");
 		driver.findElement(By.xpath("//input[@id='pass']")).clear();
 		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("123456");
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//button[@id='send2']")).click();
-		System.out.println("Login sucessfully");
+		Thread.sleep(10000);
+		String title = driver.getTitle();
+		System.out.println(title);
+
+		if (title.equals("My Account")) {
+			System.out.println("Login sucessfully");
+		} else {
+			System.out.println("Test cases failed");
+		}
 	}
+	
+	public void navigation() throws InterruptedException
+	{
+		driver.findElement(By.xpath("//a[@class='level0' and text()='Products']")).click();
+		
+	}
+
 	public static void main(String[] args) throws InterruptedException {
 		SignUp obj = new SignUp();
 		obj.register();
 		obj.login();
+		obj.navigation();
 
 	}
 }
